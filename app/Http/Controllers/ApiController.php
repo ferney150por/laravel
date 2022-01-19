fbca91cf21f0eb5cb104e93fca19194ebb510729
ferney150por <ferney@150porciento.com>
2022-01-19 16:09:00 -0500

    ejercicios

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -23,14 +23,35 @@ class ApiController extends Controller
     public function index()
     {
         $users = User::all();
-
         $usersArray = $users->toArray();
 
-        foreach ($usersArray as $key => $value) {
-            $usersArray[$key]['name'] = $usersArray[$key + 1 ]['name'] . ' Morales';
-        }
+        //debug data in variables
 
-        
+        //ORM
+
+        //length of array
+
+        //add field name with firstname concact lastname 'Rodriguez'
+
+        //encode data to json utf8
+
+        //decode data to object and array
+
+        //if user mail is correo@correo.com delete
+
+        //replace in names "a" to "o"
+
+        //add field subscribed_at with now time + 1 day in format DD-MM-AAAA
+
+        //change to DATETIME MySQL
+
+
+        //ADVANCED
+
+        //inport base64 and convert to image
+
+        //petition with Curl
+
         return response()->json($usersArray);
     }
 
